7d31c6be27dc4f79e654384a6d435dcbabd9bad0
dadiaco <duvandiaz2204@gmail.com>
2024-08-24 19:38:51 -0500

    Creación de animación para el modal de datos

--- js/linksContacto.js
+++ js/linksContacto.js
@@ -1,12 +1,25 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function() {
+    const shadow = document.getElementById('shadow');
     const openModalButton = document.getElementById('contact');
     const closeModalButton = document.getElementById('close-button');
     const modal = document.getElementById('container-contact');
 
-    openModalButton.addEventListener('click',function(){
+    function openModal() {
         modal.style.display = "block";
-    })
-    closeModalButton.addEventListener('click',function(){
+        shadow.style.display = "block";
+    }
+
+    function closeModal() {
         modal.style.display = "none";
-    })
-})
+        shadow.style.display = "none";
+    }
+
+    openModalButton.addEventListener('click', openModal);
+    closeModalButton.addEventListener('click', closeModal);
+
+    window.addEventListener('click', function(event) {
+        if (event.target === shadow) {
+            closeModal();
+        }
+    });
+});
